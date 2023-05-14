208f54a0943da1b41a4550c2823ad58e91ed009e
Nguyễn Thiện Nhân <88951651+tnhansg@users.noreply.github.com>
2023-05-14 12:24:55 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,12 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+ "protectedRoutes": [
+  '/TaiLieu',
+],
+  "protectedRoutes": [
+  '/UpRom',
+],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:nguyenthiennhan.tc@gmail.com',
@@ -54,6 +59,12 @@ module.exports = {
     {
       name: 'Facebook',
       link: 'https://www.facebook.com/tnhantl/',
+    },
+  ],
+    links: [
+    {
+      name: 'Telegram',
+      link: 'https://t.me/tnhantl/',
     },
   ],
 
